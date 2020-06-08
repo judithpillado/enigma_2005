b796cea6f64b775066645c6d4fba7dda2419dba4
Judith Pillado <judith.pillado@gmail.com>
2020-06-08 16:47:10 -0500

    Add xcryption method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,4 +4,22 @@ class Enigma
     @alphabet = ("a".."z").to_a << " "
   end
 
+  def shifts(key, offset)
+    require "pry";binding.pry
+    hash = Hash.new
+    hash[:A] = key.assign_keys[:A] + offset.assign_offsets[:A]
+    hash[:B] = key.assign_keys[:B] + offset.assign_offsets[:B]
+    hash[:C] = key.assign_keys[:C] + offset.assign_offsets[:C]
+    hash[:D] = key.assign_keys[:D] + offset.assign_offsets[:D]
+    hash
+  end
+
+  def xcryption(xcryption, message, key, date)
+    hash = Hash.new
+    hash[xcryption] = message
+    hash[:key] = key
+    hash[:date] = date
+    hash
+  end
+
 end
