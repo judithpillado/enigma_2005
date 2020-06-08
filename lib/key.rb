879b44b5a2a470a832514b644812b14dc47d5185
Judith Pillado <judith.pillado@gmail.com>
2020-06-08 15:16:22 -0500

    Add assign keys method

--- lib/key.rb
+++ lib/key.rb
@@ -18,4 +18,13 @@ class Key
     keys
   end
 
+  def assign_keys
+    hash = Hash.new
+    hash[:A] = generate_keys[0]
+    hash[:B] = generate_keys[1]
+    hash[:C] = generate_keys[2]
+    hash[:D] = generate_keys[3]
+    hash
+  end
+
 end
