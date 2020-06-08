cef18732fe83ecfd09837f61b8cfb83a5f42d1d5
Judith Pillado <judith.pillado@gmail.com>
2020-06-08 14:00:32 -0500

    Add test it exists

--- test/key_test.rb
+++ test/key_test.rb
@@ -1,3 +1,15 @@
 require './test/test_helper'
 require './lib/enigma'
 require './lib/key'
+
+class KeyTest < Minitest::Test
+
+  def setup
+    @key = Key.new
+  end
+
+  def test_it_exists
+    assert_instance_of Key, @key
+  end 
+
+end
